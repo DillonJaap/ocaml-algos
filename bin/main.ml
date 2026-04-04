b85428c40c22a25c38e2249459164f54236b5869
dillon <dillonjaap@gmail.com>
2026-04-03 20:53:53 -0600

    comments

--- bin/main.ml
+++ bin/main.ml
@@ -1,13 +1,21 @@
 open Core
 
 let insertion_sort array =
+  (* Iterate through each element starting from the second element *)
   for j = 1 to Array.length array - 1 do
+    (* Store the current element to be inserted *)
     let current_element = ref array.(j) in
+
+    (* Start from the element before the current one *)
     let i = ref (j - 1) in
+
+    (* Shift all elements greater than current_element one position to the right *)
     while !i >= 0 && array.(!i) > !current_element do
       array.(!i + 1) <- array.(!i);
       i := !i - 1
     done;
+
+    (* Insert the current element into its correct sorted position *)
     array.(!i + 1) <- !current_element
   done
 ;;
