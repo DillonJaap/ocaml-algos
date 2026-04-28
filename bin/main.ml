1dad18c641c07a52ef4808a15cc0bdf68938ef56
dillon <dillonjaap@gmail.com>
2026-04-28 08:30:01 -0600

    stuff

--- bin/main.ml
+++ bin/main.ml
@@ -1,26 +1,41 @@
-let insertion_sort array =
-  (* Iterate through each element starting from the second element *)
-  for j = 1 to Array.length array - 1 do
-    (* Store the current element to be inserted *)
-    let current_element = ref array.(j) in
+module type Sort = sig
+  val sort : int array -> int array
+end
 
-    (* Start from the element before the current one *)
-    let i = ref (j - 1) in
+module Insertion : Sort = struct
+  let sort in_array =
+    let array = Array.copy in_array in
 
-    (* Shift all elements greater than current_element one position to the right *)
-    while !i >= 0 && array.(!i) > !current_element do
-      array.(!i + 1) <- array.(!i);
-      i := !i - 1
+    (* Iterate through each element starting from the second element *)
+    for j = 1 to Array.length array - 1 do
+      (* Store the current element to be inserted *)
+      let current_element = ref array.(j) in
+
+      (* Start from the element before the current one *)
+      let i = ref (j - 1) in
+
+      (* Shift all elements greater than current_element one position to the right *)
+      while !i >= 0 && array.(!i) > !current_element do
+        array.(!i + 1) <- array.(!i);
+        i := !i - 1
+      done;
+
+      (* Insert the current element into its correct sorted position *)
+      array.(!i + 1) <- !current_element
     done;
 
-    (* Insert the current element into its correct sorted position *)
-    array.(!i + 1) <- !current_element
-  done
-;;
+    array
+  ;;
+end
 
 let () =
   let arr = [| 50; 2; 6; 9; 20; 2; 3; 4; 12; 10 |] in
-  insertion_sort arr;
+  let new_arr = Insertion.sort arr in
 
-  Format.printf "\n%a\n" Pp.int_array arr
+  Format.printf
+    "\nInsertion Sort\n%a\n%a\n"
+    Pp.int_array
+    arr
+    Pp.int_array
+    new_arr
 ;;
